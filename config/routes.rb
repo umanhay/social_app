9acb0f796406194aceb8e2282cd1db96ee826ff3
umanhay <ptown727@yahoo.com>
2018-01-27 22:30:12 -0800

    Add user controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
   root 'static_pages#home'
 
   get '/help', to: 'static_pages#help'
@@ -6,5 +8,7 @@ Rails.application.routes.draw do
   get '/about', to: 'static_pages#about'
 
   get '/contact', to: 'static_pages#contact'
+
+  get '/signup', to: 'users#new'
 
 end
